bb00f1d74ffbd258100468d477b1e5bdf6413090
Shiham <s36651786@gmail.com>
2024-06-30 21:19:33 +0600

    Not-found route explained

--- src/app/products/[productsId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productsId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import {notFound} from "next/navigation"
+
 const ReviewsDetails = ({params}: {productsId: string; reviewId: string}) => {
+	if (parseInt(params.reviewId) > 1000) {
+		notFound()
+	}
 	return (
 		<div>
 			<h1>
