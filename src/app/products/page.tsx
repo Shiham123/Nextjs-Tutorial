fac70247b822c17a2359633e0c81f20713d4a48d
Shiham <s36651786@gmail.com>
2024-07-01 18:43:30 +0600

    dynamic route and replace explained

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,12 +2,20 @@ import Link from "next/link"
 import React from "react"
 
 const ProductList = () => {
+	const productId = 100
+
 	return (
 		<div>
 			<h1>products list</h1>
 			<h1>Product one</h1>
 			<h1>Product two</h1>
-			<h1>Product three</h1>
+			<Link href="/products/3" replace>
+				<h1>Product three</h1>
+			</Link>
+
+			<h2>
+				<Link href={`products/${productId}`}>Dynamic route</Link>
+			</h2>
 
 			<h2>
 				<Link href="/">Back to home page</Link>
