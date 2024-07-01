8b01bccce44a0708fe36f76d06ea0f66cbc57aca
Shiham <s36651786@gmail.com>
2024-07-01 18:54:10 +0600

    understand link and style it

--- src/app/(auth)/(with-auth-layout)/register/page.tsx
+++ src/app/(auth)/(with-auth-layout)/register/page.tsx
@@ -1,5 +1,36 @@
+"use client"
+
+import Link from "next/link"
+import {usePathname} from "next/navigation"
+
+const navLinks = [
+	{name: "Register", href: "/register"},
+	{name: "Login", href: "/login"},
+	{name: "forgot", href: "/forgot"},
+]
+
 const Register = () => {
-	return <h1>This is register</h1>
+	const pathname = usePathname()
+
+	return (
+		<>
+			<h1>This is register</h1>
+
+			{navLinks.map((link) => {
+				const isActive = pathname.startsWith(link.href)
+
+				return (
+					<Link
+						href={link.href}
+						key={link.name}
+						className={isActive ? "text-yellow-500" : "text-red-700"}
+					>
+						<h1>{link.name}</h1>
+					</Link>
+				)
+			})}
+		</>
+	)
 }
 
 export default Register
