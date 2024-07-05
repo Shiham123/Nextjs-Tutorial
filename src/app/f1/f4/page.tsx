b82845b3dc83e817f420fbd6e73bee1104150c73
Shiham <s36651786@gmail.com>
2024-07-05 16:29:39 +0600

    any nested folder intercepting

--- src/app/f1/f4/page.tsx
+++ src/app/f1/f4/page.tsx
@@ -6,6 +6,7 @@ const FolderFourPage = () => {
 		<div>
 			<h1>folder four page</h1>
 			<Link href="/f1/f3">Navigate to folder three page</Link>
+			<Link href="/about">About</Link>
 		</div>
 	)
 }
