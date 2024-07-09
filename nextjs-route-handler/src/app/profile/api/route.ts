4deea466c415b97c08f891dc01e19f652f2084c0
Shiham <s36651786@gmail.com>
2024-07-09 22:05:21 +0600

    cookie set and get explained

--- nextjs-route-handler/src/app/profile/api/route.ts
+++ nextjs-route-handler/src/app/profile/api/route.ts
@@ -4,8 +4,11 @@ import {headers} from "next/headers"
 export async function GET(request: NextRequest) {
 	const requestHeaders = new Headers(request.headers)
 	const headerList = headers()
+	const theme = request.cookies.get("theme")
 
 	console.log(requestHeaders.get("Authorization"))
+	console.log(headerList.get("Authorization"))
+	console.log(theme)
 
-	return new Response("<h1>profile api data</h1>", {headers: {"Content-Type": "text/html"}})
+	return new Response("<h1>profile api data</h1>", {headers: {"Content-Type": "text/html", "Set-Cookie": "dark"}})
 }
